19faf515b16d29294ce6bf1163fe4ec1925fb006
Victoria Jackson <victoriaxjacksoncma@gmail.com>
2023-02-13 05:59:19 -0800

    A function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)

---
+++ codeWars2-13-23.js
@@ -0,0 +1,8 @@
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+  let n1 = Number(a)
+  let n2 = Number(b)
+  let sum = n1 + n2
+  return sum.toString()
+}
