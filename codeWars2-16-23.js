61768fe7ac9bf820a261ce4ec3560fb4dc41591a
Victoria Jackson <victoriaxjacksoncma@gmail.com>
2023-02-16 17:16:23 -0800

    Daily codewars challenge

---
+++ codeWars2-16-23.js
@@ -0,0 +1,7 @@
+//Instructions: Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//Solution:
+
+function removeExclamationMarks(s) {
+    return s.replaceAll('!', '');
+}
