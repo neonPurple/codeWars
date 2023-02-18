3ddebead1dc550ba1e9cf35abdc13894dbafdd65
Victoria Jackson <victoriaxjacksoncma@gmail.com>
2023-02-18 15:25:36 -0800

    Daily Challenge

---
+++ codeWars2-18-23.js
@@ -0,0 +1,14 @@
+//The challenge: Thie following code does not execute properly. Try to figure out why.
+//Given code:
+
+
+// function multiply(a,b){
+//   a*b
+// }
+
+
+//My Solution:
+
+function multiply(a,b){
+  return a*b
+}
