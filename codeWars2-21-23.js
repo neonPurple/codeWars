83c2c9a1d8ca9574b20987f89a3402b3e87d9bbe
Victoria Jackson <victoriaxjacksoncma@gmail.com>
2023-02-21 05:53:24 -0800

    Daily Challenge

---
+++ codeWars2-21-23.js
@@ -0,0 +1,21 @@
+//The challenge: Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+//My solution:
+
+function basicOp(operation, value1, value2)
+{
+  switch (operation){
+      case '+':
+        return value1 + value2;
+      case '-':
+        return value1 - value2;
+      case '*':
+        return value1 * value2;
+      case '/':
+        return value1 / value2;
+      default:
+        return 'Invalid Input';
+  }
+}
