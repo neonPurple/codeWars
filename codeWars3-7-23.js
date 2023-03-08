abbc1a8a9cae10554de35b68dfbf51e6fc4ce594
Victoria Jackson <victoriaxjacksoncma@gmail.com>
2023-03-07 21:45:43 -0800

    Daily Challenge

---
+++ codeWars3-7-23.js
@@ -0,0 +1,9 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+//Solution:
+
+function sumMix(x){
+  return x.reduce((acc, curr)=> acc + Number(curr), 0)
+}
